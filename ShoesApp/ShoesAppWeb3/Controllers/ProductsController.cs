710a2ed6fb372090f21caa7cde00beb0761d99e5
briseidy-torres-velez <58743853+briseidy-torres-velez@users.noreply.github.com>
2020-01-08 15:15:33 -0600

    insert, update,delete

--- ShoesApp/ShoesAppWeb3/Controllers/ProductsController.cs
+++ ShoesApp/ShoesAppWeb3/Controllers/ProductsController.cs
@@ -18,7 +18,7 @@ namespace ShoesAppWeb3.Controllers
         public ActionResult Products()
         {
             DataProductsEntities DTE = new DataProductsEntities();
-            
+
             return View(DTE.BRTV_Show());
         }
 
@@ -35,16 +35,16 @@ namespace ShoesAppWeb3.Controllers
         {
             try
             {
-                
+
                 CapaDatos capaDatos = new CapaDatos();
                 capaDatos.Insert(AddET);
-               // DataProductsEntities DTE = new DataProductsEntities();
+                // DataProductsEntities DTE = new DataProductsEntities();
 
-               // AddET.DateUpdate = DateTime.Now;
+                // AddET.DateUpdate = DateTime.Now;
 
-               // DTE.BRTV_Insert(AddET.IdType, AddET.IdColor, AddET.IdBrand, AddET.IdProvider, AddET.IdCatalog, AddET.Title, AddET.Nombre, AddET.Description,
-               //AddET.Observations, AddET.PriceDistributor, AddET.PriceClient, AddET.PriceMember, AddET.IsEnabled, AddET.Keywords, AddET.DateUpdate);
-               // //products.DateUpdate = DateTime.Now;
+                // DTE.BRTV_Insert(AddET.IdType, AddET.IdColor, AddET.IdBrand, AddET.IdProvider, AddET.IdCatalog, AddET.Title, AddET.Nombre, AddET.Description,
+                //AddET.Observations, AddET.PriceDistributor, AddET.PriceClient, AddET.PriceMember, AddET.IsEnabled, AddET.Keywords, AddET.DateUpdate);
+                // //products.DateUpdate = DateTime.Now;
 
                 return RedirectToAction("Products");
 
@@ -57,5 +57,94 @@ namespace ShoesAppWeb3.Controllers
                 throw;
             }
         }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+
+
+
+            using (var db = new DataProductsEntities())
+            {
+                BRTV_Show_Result x = db.BRTV_Show().Where(a => a.Id == id).FirstOrDefault();
+
+                return View();
+            }
+
+
+        }
+
+        [HttpPost]
+
+        public ActionResult Edit(AddProdET AddET)
+        {
+            try
+            {
+
+                CapaDatos capaDatos = new CapaDatos();
+                capaDatos.Update(AddET);
+
+
+                return RedirectToAction("Products");
+
+            }
+            catch (Exception)
+            {
+
+
+
+                throw;
+            }
+        }
+       
+        public ActionResult Delete(int id)
+        {
+
+
+            using (var db = new DataProductsEntities())
+            {
+                BRTV_Show_Result x = db.BRTV_Show().Where(a => a.Id == id).FirstOrDefault();
+
+                return View(x);
+            }
+
+
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+
+            try
+            {
+
+                using (var db = new DataProductsEntities())
+                {
+                    BRTV_Show_Result x = db.BRTV_Show().Where(a => a.Id == id).FirstOrDefault();
+                    CapaDatos capaDatos = new CapaDatos();
+                    capaDatos.Delete(id);
+
+
+
+
+                    return RedirectToAction("Products");
+                }
+
+            }
+            catch (Exception)
+            {
+
+
+
+                throw;
+            }
+        }
+        public ActionResult Search(string name)
+        {
+
+            DataProductsEntities DTE = new DataProductsEntities();
+            
+            return View(DTE.BRTV_SearchName(name));
+        }
+
     }
 }
